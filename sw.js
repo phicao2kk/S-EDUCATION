c73267a3c70b6a2fc305e18547ab0f4340b791d9
phicao2kk <phicao2kk@gmail.com>
2026-05-02 02:26:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = 's-education';
+// 1. Mỗi khi bạn sửa code HTML/CSS, hãy tăng số version này lên (v1 -> v2)
+const CACHE_NAME = 's-education-v2'; 
 const assets = [
   './',
   './index.html',
   './manifest.json'
 ];
 
+// Cài đặt và ép Service Worker mới kích hoạt ngay lập tức
 self.addEventListener('install', e => {
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
@@ -13,10 +16,22 @@ self.addEventListener('install', e => {
   );
 });
 
+// Dọn dẹp cache cũ khi phiên bản mới (v2, v3...) được cài đặt
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Chiến lược: Network First (Lấy mới trước, dự phòng cache sau)
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
     })
   );
 });
